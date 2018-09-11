c41398ef5c51f508cd14e31837732909dc555c80
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-09-10 23:37:09 -0400

    Create a Complex JSX Element

--- reactJS.js
+++ reactJS.js
@@ -1,2 +1,15 @@
 // Create a Simple JSX Element
 const JSX = <h1>Hello JSX!</h1>
+
+
+// Create a Complex JSX Element
+const JSX =
+    <div>
+        <h1>Header</h1>
+        <p>Paragraph</p>
+        <ul>
+            <li>list 1</li>
+            <li>list 2</li>
+            <li>list 3</li>
+        </ul>>
+</div>
